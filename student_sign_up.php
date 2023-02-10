2c601c1b91760d6fda50781d6a19f79195978927
Ramsey Holeman <ramsey.holeman.19@cnu.edu>
2023-02-10 02:48:56 -0500

    Added to major and minor selection of form

--- student_sign_up.php
+++ student_sign_up.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sign Up</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="design.css">
     <h1>Sign Up</h1>
   </head>
   <body>
@@ -29,13 +29,27 @@
       <label for="email">Expected Graduation Year:</label>
       <input type="text" id="email" name="email" required><br><br>
 
-      <!-- Add a dropdown menu for Major -->
-      <label for="major">Major:</label>
+      <!-- Major selection -->
+      <label for="major">Select your Major:</label>
+      <select name="major" id="major" required>
+        <option value="pick_one">*Select a Major*</option>
+        <option value="Physics">Physics</option>
+        <option value="Computer Engineering">Computer Engineering</option>
+        <option value="Computer Science">Computer Science</option>
+        <option value="Cybersecurity">Cybersecurity</option>
+        <option value="Electrical Engineering">Electrical Engineering</option>
+        <option value="Information Science">Information Science</option>
+      </select><br><br>
+
+      <label for="major">Enter your second Minor (If applicable):</label>
       <input type="text" id="major" name="major" required><br><br>
 
-      <!-- Add a dropdown menu for Minor -->
-      <label for="minor">Minor:</label>
+      <!-- Minor selection -->
+      <label for="minor">Enter your Minor:</label>
       <input type="text" id="minor" name="minor" required><br><br>
+
+      <label for="minor">Enter your second Minor (If applicable):</label>
+      <input type="text" id="minor" name="minor"><br><br>
 
       <input type="submit" value="Login">
     </form>

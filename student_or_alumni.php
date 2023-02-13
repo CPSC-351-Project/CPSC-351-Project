1b5ea44c11705334df76d148a8fb66988dcc767d
Ramsey Holeman <ramsey.holeman.19@cnu.edu>
2023-02-13 18:57:48 -0500

    Working on page connectivity

--- student_or_alumni.php
+++ student_or_alumni.php
@@ -12,25 +12,32 @@
   <p style="text-align: center;">Pick a user</p>
   <div class="login-form">
     <form action="" style="display: inline-block;">
-      <input type="radio" id="student" name="student" value="Student" required>
+      <input type="radio" id="student" name="user" value="Student">
       <label for="student">Student</label><br><br>
 
-      <input type="radio" id="alumni" name="alumni" value="Alumni" required>
+      <input type="radio" id="alumni" name="user" value="Alumni">
       <label for="alumni">Alumni</label><br><br>
 
       <input type="submit" value="Sign Up">
-      
-      <?php
-      $student = $_POST["student"];
-      $alumni = $_POST["alumni"];
-
-      /*
-      if (isset($student)){
-          header();
+    </form>
+    
+    
+    <?php
+      /*if(isset($_POST['user'])) {
+        $selected_option = $_POST['user'];
+        if($selected_option == 'Student') {
+          header('Location:student_sign_up.php');
+          exit;
+        } elseif($selected_option == 'Alumni') {
+          header('Location:alumni_sign_up.php');
+          exit;
+        }
+        else {
+        header('Location:student_or_alumni.php');
+        exit;
+        }
       }*/
       ?>
-      
-    </form>
   </div>
   </body>
 </html>

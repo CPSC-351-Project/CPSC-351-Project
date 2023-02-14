33e5d0aaf16031e7549471fe54e77ed4f8dbdb82
bjahchi <jahchi.burford.21@cnu.edu>
2023-02-14 17:32:38 -0500

    commit commit

--- login.php
+++ login.php
@@ -10,8 +10,43 @@ if (isset($_POST['submit'])) {
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="design.css">
   <title>Login Here</title>
+  <header>
+          <link rel="stylesheet" href="design.css">
+          <div id="wrap">
+              <ul class="navbar">
+                <li><a href="index.html">Home</a></li>
+                <li>
+                  <a href="#">Job</a>
+                  <ul>
+                    <li><a href="Find Jobs.html">Find Jobs</a></li>
+                    <li><a href="Add Job.html">Post a Job</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Networking</a>
+                  <ul>
+                    <li><a href="#">Advice Forum</a></li>
+                    <li><a href="#">Events Page</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Account</a>
+                  <ul>
+                    <li><a href="student_or_alumni.php">Create Account</a></li>
+                    <li><a href="login.php">Manage Account</a></li>
+                    <li><a href="#">Notifications</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            
+            
+            
+          <!-- <a href="https://cnu.edu/"><img src="cnu.png" style=float:left;width:27% ></a> -->
+          <h1>AlumniReach</h1>
+      </header>
 </head>
 <body>
   <div class="login-form">
@@ -28,4 +63,7 @@ if (isset($_POST['submit'])) {
 
 
 
-</body></html>
+</body>
+
+
+</html>

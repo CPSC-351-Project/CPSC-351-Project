820841a78a9b5c8091a872753dbba897cb8909a5
manonithao <119448395+manonithao@users.noreply.github.com>
2023-02-16 14:03:28 -0500

    added the navbar

--- login.php
+++ login.php
@@ -6,12 +6,45 @@ if (isset($_POST['submit'])) {
 
 }
 
-?>
+echo '
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <title>Login Here (test code here)</title>
+<header>
+        <link rel="stylesheet" href="design.css">
+        <div id="wrap">
+            <ul class="navbar">
+              <li><a href="index.html">Home</a></li>
+              <li>
+                <a href="#">Job</a>
+                <ul>
+                  <li><a href="Find Jobs.html">Find Jobs</a></li>
+                  <li><a href="Add Job.html">Post a Job</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Networking</a>
+                <ul>
+                  <li><a href="#">Advice Forum</a></li>
+                  <li><a href="#">Events Page</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Account</a>
+                <ul>
+                  <li><a href="login.php">Create Account</a></li>
+                  <li><a href="#">Manage Account</a></li>
+                  <li><a href="#">Notifications</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          
+        <!-- <a href="https://cnu.edu/"><img src="cnu.png" style=float:left;width:27% ></a> -->
+        <h1>Website Title</h1>
+    </header>
+  <link rel="stylesheet" href="design.css">
+  <title>Login Here</title>
 </head>
 <body>
   <div class="login-form">
@@ -29,3 +62,5 @@ if (isset($_POST['submit'])) {
 
 
 </body></html>
+';
+?>

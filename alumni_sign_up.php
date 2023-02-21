d1c8505521f19ba0e8d1f568970d00adf711accf
Ramsey Holeman <ramsey.holeman.19@cnu.edu>
2023-02-21 04:06:05 -0500

    Made a confirmation page for alumni

--- alumni_sign_up.php
+++ alumni_sign_up.php
@@ -11,7 +11,7 @@
   <body style="text-align: center;">
   <p style="text-align: center;">Sign up page for alumni users</p>
   <div class="login-form">
-    <form action="alumni_sign_up.php" style="display: inline-block;">
+    <form action="account_confirm_for_alumni.php" style="display: inline-block;">
       <label for="firstname">First Name</label>
       <input type="text" id="firstname" name="firstname" required><br><br>
       
@@ -27,36 +27,41 @@
       <input type="submit" value="Sign Up">
     </form>
     <?php
-    // SQL Database connection
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "351project";
+    // // SQL Database connection
+    // $server = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $dbname = "351project";
 
-    $conn = mysqli_connect($server, $username, $password, $dbname);
+    // $conn = mysqli_connect($server, $username, $password, $dbname);
 
-    // Check if the connection was successful
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+    // // Check if the connection was successful
+    // if (!$conn) {
+    //     die("Connection failed: " . mysqli_connect_error());
+    // }
 
-    // Collect information from form
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-    $grad_year = $_POST['grad_year'];
+    // // Collect information from form
+    // $firstname = $_POST['firstname'];
+    // $lastname = $_POST['lastname'];
+    // $email = $_POST['email'];
+    // $grad_year = $_POST['grad_year'];
 
-    // Escape the username and password to prevent SQL injection attacks
-    $firstname = mysqli_real_escape_string($conn, $firstname);
-    $lastname = mysqli_real_escape_string($conn, $lastname);
-    $email = mysqli_real_escape_string($conn, $email);
-    $grad_year  = mysqli_real_escape_string($conn, $grad_year);
+    // // Escape the username and password to prevent SQL injection attacks
+    // $firstname = mysqli_real_escape_string($conn, $firstname);
+    // $lastname = mysqli_real_escape_string($conn, $lastname);
+    // $email = mysqli_real_escape_string($conn, $email);
+    // $grad_year  = mysqli_real_escape_string($conn, $grad_year);
     
-    // Add the form information to the database
-    $insert_firstname = "INSERT INTO `alumni_accounts`(`firstname`) VALUES ('$firstname');";
-    $insert_lastname = "INSERT INTO `alumni_accounts`(`lastname`) VALUES ('$lastname');";
-    $insert_email = "INSERT INTO `alumni_accounts`(`email`) VALUES ('$email');";
-    $insert_year = "INSERT INTO `alumni_accounts`(`grad_year`) VALUES ('$grad_year');"; 
+    // // Add the form information to the database
+    // $insert_firstname = "INSERT INTO `alumni_accounts`(`firstname`) VALUES ('$firstname');";
+    // mysqli_query($conn, $insert_firstname);
+    // $insert_lastname = "INSERT INTO `alumni_accounts`(`lastname`) VALUES ('$lastname');";
+    // mysqli_query($conn, $insert_lastname);
+    // $insert_email = "INSERT INTO `alumni_accounts`(`email`) VALUES ('$email');";
+    // mysqli_query($conn, $insert_email);
+    // $insert_year = "INSERT INTO `alumni_accounts`(`grad_year`) VALUES ('$grad_year');";
+    // mysqli_query($conn, $insert_year);
+    // $conn->close();
     ?>
   </div>
   </body>

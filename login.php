a4764f887cd3d371e9cd2212b7c218e12392e195
Durenr <ryen.duren.19@cnu.edu>
2023-02-28 16:47:54 -0500

    Update login.php

--- login.php
+++ login.php
@@ -72,7 +72,7 @@ if (isset($_POST['submit'])) {
     $server = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "351project";
+    $dbname = "alumnireach";
 
     $conn = mysqli_connect($server, $username, $password, $dbname);
 

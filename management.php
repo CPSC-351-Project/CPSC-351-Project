c6d98ed62728e14ab5258ea2d26392f48921afa5
manonithao <119448395+manonithao@users.noreply.github.com>
2023-03-26 18:55:35 -0400

    worked on the management page and added the edit_profile

--- management.php
+++ management.php
@@ -1,77 +1,71 @@
 <?php
 session_start();
-    include "connection.php";
-    include "functions.php";
-    $user_data = check_login($conn);
-
+include "connection.php";
+include "functions.php";
+$user_data = check_login($conn);
 ?>
 
 <!DOCTYPE html>
 <html>
-<title>Alumni Reach</title>
-<head>    
-  <header>
+<head>
+    <title>Alumni Reach</title>
     <link rel="stylesheet" href="design.css">
-    <div id="wrap">
-        <ul class="navbar">
-            <li><a href="index.php">Home</a></li>
-            <li>
-                <a href="#">Job</a>
-                <ul>
-                    <li><a href="Find Jobs.php">Find Jobs</a></li>
-                    <li><a href="Add Job.php">Post a Job</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Networking</a>
-                <ul>
-                    <li><a href="#">Advice Forum</a></li>
-                    <li><a href="#">Events Page</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="login.php">Account</a>
-                <ul>
-                    <li><a href="login.php">Login/Create Account</a></li>
-                    <li><a href="management.php">Manage Account</a></li>
-                    <li><a href="account.php">Notifications</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="logout.php">Logout</a>
-            </li>
-        
-        </ul>
-      </div>
-      <!-- <a href="https://cnu.edu/"><img src="cnu.png" style=float:left;width:27% ></a> -->
-    <h1>AlumniReach</h1>      
-  </header>
 </head>
-<h4> Manage Your Account Here!</h4>
 <body>
-    <div align="center">
-        <div class= "row">
-            <form action="">
-                <div class="form-group">
-                    <label for="firstname">First Name</label>
-                    <input type="text" name="firstname" class="form-control" value="<?php echo $user_data['first_name'];?>"><br><br>
-                    <label for="lastname">Last Name</label>
-                    <input type="text" name="lastname" class="form-control" value="<?php echo $user_data['last_name'];?>"><br><br>
-                    <label for="email">Email</label>
-                    <input type="text" name="email" class="form-control" value="<?php echo $user_data['email'];?>"><br><br>
-                    <label for="job">Position title</label>
-                    <input type="text" name="job" class="form-control" value="<?php echo $user_data['job'];?>"><br><br>
-                    <label for="bio">Bio</label>
-<<<<<<< HEAD
-                    <input type="text" name="bio" class="form-control" value="<?php echo $user_data['bio'];?>"><br>
-                    <input type="submit" value="Add contact information">
-                    <input type="submit" value="Update changes">
+    <header>
+        <div id="wrap">
+            <ul class="navbar">
+                <li><a href="index.php">Home</a></li>
+                <li>
+                    <a href="#">Job</a>
+                    <ul>
+                        <li><a href="Find Jobs.php">Find Jobs</a></li>
+                        <li><a href="Add Job.php">Post a Job</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Networking</a>
+                    <ul>
+                        <li><a href="#">Advice Forum</a></li>
+                        <li><a href="#">Events Page</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="login.php">Account</a>
+                    <ul>
+                        <li><a href="login.php">Login/Create Account</a></li>
+                        <li><a href="management.php">Manage Account</a></li>
+                        <li><a href="account.php">Notifications</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+        <h1>AlumniReach</h1>
+    </header>
 
-=======
-                    <input type="text" name="bio" class="form-control" value="<?php echo $user_data['bio'];?>"><br><br>
-                    <input type="submit" value="Add Information"><br>
-                    <input type="submit" value="Update Information"><br>
->>>>>>> 71204cbc5f8a80432cf0f3fd856f82b185121524
+    <div align="center">
+        <div class="row">
+            <form action="" method="POST" style="max-width: 600px; margin: 0 auto; padding: 20px; padding-top: 0px; background-color: white;">
+                <div class="form-group">
+                    <h1>Manage your Account here</h1>
+                    <label for="firstname">First Name</label>
+                    <input type="text" name="firstname" class="form-control" value="<?php echo $user_data['first_name'];?>" readonly><br><br>
+                    <label for="lastname">Last Name</label>
+                    <input type="text" name="lastname" class="form-control" value="<?php echo $user_data['last_name'];?>" readonly><br><br>
+                    <label for="email">Email</label>
+                    <input type="text" name="email" class="form-control" value="<?php echo $user_data['email'];?>" readonly><br><br>
+                    <label for="job">Position title</label>
+                    <input type="text" name="job" class="form-control" value="<?php echo $user_data['job'];?>" readonly><br><br>
+                    <label for="bio">Bio</label>
+                    <textarea name="bio" class="form-control" rows="2"><?php echo $user_data['bio'];?></textarea><br><br>
+                    <label for="phone">Phone number</label>
+                    <input type="text" name="phone" class="form-control" value="<?php echo $user_data['phone_num'];?>" readonly><br><br>
+                    <label for="address">Address</label>
+                    <input type="text" name="address" class="form-control" value="<?php echo $user_data['address'];?>" readonly><br><br>
+                    <a href="edit_profile.php"><input type="button" name="edit_profile" value="Edit Profile">
                 </div>
             </form>
         </div>

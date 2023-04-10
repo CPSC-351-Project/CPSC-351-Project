c84c7ccec92e2f55d70b445e5588b2b9e1732de7
Ramsey Holeman <ramsey.holeman.19@cnu.edu>
2023-04-10 15:07:22 -0400

    fixed connection

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 // Database connection
 $hostname = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 $db = "alumnireach";
 $conn = new mysqli($hostname, $username, $password, $db) or die("Connect failed: %s\n". $conn -> error);
 
